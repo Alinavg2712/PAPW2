3335555310f6ad84f84a64cd22fe4064dea93608
LAPTOP-36OQ8LE1\Alina <alivg1992@gmail.com>
2019-03-10 14:39:52 -0600

    landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,177 @@
+<!--A Design by W3layouts
+   Author: W3layout
+   Author URL: http://w3layouts.com
+   License: Creative Commons Attribution 3.0 Unported
+   License URL: http://creativecommons.org/licenses/by/3.0/
+   -->
+   <!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>Dragon Hunt a Games Category Bootstrap responsive Website Template | Home :: w3layouts</title>
+      <!--meta tags -->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="keywords" content="Dragon Hunt Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+         Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+      <script>
+         addEventListener("load", function () {
+         	setTimeout(hideURLbar, 0);
+         }, false);
+         
+         function hideURLbar() {
+         	window.scrollTo(0, 1);
+         }
+      </script>
+      <!--//meta tags ends here-->
+      <!--booststrap-->
+      <link href="{{ URL::asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" media="all">
+      <!--//booststrap end-->
+      <!-- font-awesome icons -->
+      
+      <link  href="{{ URL::asset('css/fontawesome-all.min.css')}}" rel="stylesheet" type="text/css" media="all">
+      <!-- //font-awesome icons -->
+      <!-- Nav-CSS -->	
+      <link href="{{ URL::asset('css/nav.css')}}" rel="stylesheet" type="text/css" media="all" />
+      <script src="{{ URL::asset('js/modernizr.custom.js')}}"></script>
+      <!-- //Nav-CSS -->	
+      <!-- banner -->	
+      <link rel="stylesheet" type="text/css"  href="{{ URL::asset('css/uncover.css')}}" />
+      <!--//banner -->	
+      <!--stylesheets-->
+      <link  href="{{ URL::asset('css/style.css')}}" rel='stylesheet' type='text/css' media="all">
+      <!--//stylesheets-->
+      <link href="//fonts.googleapis.com/css?family=Cinzel+Decorative:400,700" rel="stylesheet">
+      <link href="//fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
+      <link href="//fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
+   </head>
+   <body>
+      <div class="header-outs" id="home">
+         <div class="header-w3layouts">
+            <div class="container">
+               <div class="right-side">
+                  <p>
+                     <button id="trigger-overlay" type="button">
+                     <span class="fa fa-bars" aria-hidden="true"></span>
+                     </button>
+                  </p>
+               </div>
+               <!-- open/close -->
+               <div class="overlay overlay-hugeinc">
+                  <button type="button" class="overlay-close">Close</button>
+                  <nav>
+                     <ul>
+                        <li><a href="index.html" >Home</a></li>
+                        <li><a href="about.html" >About</a></li>
+                        <li><a href="history.html" >History</a></li>
+                       
+                     </ul>
+                  </nav>
+               </div>
+               <div class="hedder-logo">
+                  <h1><a href="index.html">   
+                     <img src="{{ URL::asset('images/emoji2-01.png')}}" class="img-fluid" alt="Responsive image">Bmbzld</a>
+                  </h1>
+               </div>
+               <!-- /open/close -->
+               <!-- /navigation section -->
+            </div>
+            <div class="clearfix"> </div>
+         </div>
+         <!--banner-->
+         <div class="slides text-center">
+            <div class="slide slide--current one-img ">
+               <div class="slider-up">
+                  <h4>Bmbzld</h4>
+                  <h5>Bamboozled</h5>
+                
+                
+               </div>
+               <div class="slide__img">
+               </div>
+            </div>
+            <div class="slide two-img">
+               <div class="slider-up">
+               <h4>Bmbzld</h4>
+                  <h5>Bamboozled</h5>
+                 
+               </div>
+               <div class="slide__img"></div>
+            </div>
+            <div class="slide three-img">
+               <div class="slider-up">
+               <h4>Bmbzld</h4>
+                  <h5>Bamboozled</h5>
+                 
+               </div>
+               <div class="slide__img "></div>
+            </div>
+            <div class="slide four-img">
+               <div class="slider-up">
+               <h4>Bmbzld</h4>
+                  <h5>Bamboozled</h5>
+                  
+               </div>
+               <div class="slide__img "></div>
+            </div>
+            <div class="slide five-img">
+               <div class="slider-up">
+               <h4>Bmbzld</h4>
+                  <h5>Bamboozled</h5>
+                
+               </div>
+               <div class="slide__img "></div>
+            </div>
+            <div class="slide six-img">
+               <div class="slider-up">
+               <h4>Bmbzld</h4>
+                  <h5>Bamboozled</h5>
+                 
+               </div>
+               <div class="slide__img "></div>
+            </div>
+            <div class="clearfix"></div>
+         </div>
+         <ul class="pagination">
+            <li><span class="pagination__item"> </span></li>
+            <li><span class="pagination__item"> </span></li>
+            <li><span class="pagination__item"> </span></li>
+            <li><span class="pagination__item"> </span></li>
+            <li><span class="pagination__item"> </span></li>
+            <li><span class="pagination__item"> </span></li>
+         </ul>
+         <div class="clearfix"></div>
+      </div>
+      <!--//banner-->
+      <!--Footer -->   
+      <footer class="py-lg-4 py-md-3 py-sm-3 py-3">
+         <div class="icons text-center p-3">
+            <ul>
+               <li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+               <li><a href="#"><span class="fas fa-envelope"></span></a></li>
+               <li><a href="#"><span class="fas fa-rss"></span></a></li>
+               <li><a href="#"><span class="fab fa-vk"></span></a></li>
+            </ul>
+         </div>
+         <div class="footer-below text-center">
+            <p>©2018 Dragon Hunt. All Rights Reserved | Design by <a href="http://www.W3Layouts.com" target="_blank">W3Layouts</a></p>
+         </div>
+      </footer>
+      <!-- //Footer -->
+      <!-- js working-->
+      <script  src="{{ URL::asset('js/jquery-2.2.3.min.js')}}"></script>
+      <!--//js working-->
+      <!-- For-Banner -->
+      <script src="{{ URL::asset('js/imagesloaded.pkgd.min.js')}}"></script>
+      <script src="{{ URL::asset('js/anime.min.js')}}"></script>
+      <script src="{{ URL::asset('js/uncover.js')}}"></script>
+      <script src="{{ URL::asset('js/demo1.js')}}"></script>
+      <!-- //For-Banner -->
+      <!--nav menu-->
+      <script src="{{ URL::asset('js/classie.js')}}"></script>
+      <script src="{{ URL::asset('js/demonav.js')}}"></script>
+      <!-- //nav menu-->
+      <!-- bootstrap working-->
+      <script src="{{ URL::asset('js/bootstrap.min.js')}}"></script>
+      <!-- // bootstrap working--> 
+   </body>
+</html>
