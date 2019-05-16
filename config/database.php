c1fa9ccb2195854fd9b13b4f5ef97b45c83d262b
LAPTOP-36OQ8LE1\Alina <alivg1992@gmail.com>
2019-05-16 11:27:56 -0500

    test

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => 'gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => 'v0ebnq0lf8qu47v2',
+            'username' => 'k71xn767ic4ap88m',
+            'password' => 'zfhtqeu1xculgtjn',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
